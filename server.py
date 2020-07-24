a5015b365b10aa807c000802fe11f9e5a49d46a0
Anastasia Correa <correa.anastasia7@gmail.com>
2020-07-24 07:14:26 +0000

    Fixed routes and intake forms

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "blahhhhhhhh"
 
 @app.route('/')
 def show_homepage():
+    
     return render_template('homepage.html')
 
 ###############################
@@ -16,11 +17,28 @@ def show_homepage():
 
 @app.route('/form')
 def show_form():
-    pass
+    
+    return render_template('form.html')
 
 @app.route('/results')
 def show_results():
-    pass
+
+    compliment = request.args.getlist("choosy")
+    compliment = ", ".join(compliment)
+    
+    return render_template('results.html', compliment = compliment)
+
+@app.route('/save-name')
+def save_name():
+
+    name = request.args.get("name")
+
+    if 'name' in session:
+        name = session['name'] 
+    else:
+        name = session['name'] = {}
+
+    return redirect('http://0.0.0.0:5000/form')
 
 
 if __name__ == "__main__":
